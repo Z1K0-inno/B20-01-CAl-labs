5b829cc37b2d425b7c2123826354057537b60ee7
Z1K0-inno <kanabekovzadr@gmail.com>
2020-10-01 20:14:30 +0600

    Week 5

---
+++ week5/ex5/ex5.v
@@ -0,0 +1,42 @@
+module ex3
+(
+input s,
+input r,
+output q,
+output q_n
+);
+assign q = ~ ( r | q_n );
+assign q_n = ~ ( s | q );
+endmodule
+
+module ex4
+(
+input clk ,
+input d,
+output q,
+output q_n
+);
+wire r = ~d & clk;
+wire s = d & clk ;
+ex3 ex3 (s, r, q, q_n);
+endmodule
+
+module ex5 (
+input clk ,
+input d,
+output q,
+output q_n
+);
+wire n1;
+ex4 master (
+. clk ( ~ clk ),
+.d ( d ),
+.q ( n1 )
+);
+ex4 slave (
+. clk ( clk ),
+.d ( n1 ),
+.q ( q ),
+.q_n ( q_n )
+);
+endmodule
